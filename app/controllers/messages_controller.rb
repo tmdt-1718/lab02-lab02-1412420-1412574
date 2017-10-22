0c6de33e6ae1d57822ea5590ac126cb29fb392c6
Huuphuoc <phanphuocdt@gmail.com>
2017-10-23 00:11:49 +0700

    send message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,30 @@ class MessagesController < ApplicationController
     message.read = true
     message.read_at = Time.zone.now
     message.save 
-    message_response = {
+    message_response = _build_message_response(message)
+    render json: { MESSAGE => message_response, ok: true }, status: STATUS_OK
+  end
+
+  def message
+    message = UserMessage.find(params[:id])
+    message_response = _build_message_response(message)
+    render json: { MESSAGE => message_response, ok: true }, status: STATUS_OK
+  end 
+
+  def send_message
+    user_ids = params[:users]
+    content = params[:content]
+    sender_id = current_user.id
+    user_ids.each do |receiver_id|
+      message = UserMessage.new(sender_id: sender_id, receiver_id: receiver_id, content: content)
+      message.save
+    end 
+    render json: { ok: true }, status: STATUS_OK
+  end 
+
+  private 
+  def _build_message_response(message)
+    {
       id: message.id,
       content: message.content,
       sent_at: message.sent_at,
@@ -15,6 +38,5 @@ class MessagesController < ApplicationController
       read_at: message.read_at,
       sent_ago: message.sent_at.strftime('%c')
     }
-    render json: { MESSAGE => message_response, ok: true }, status: STATUS_OK
-  end
+  end 
 end
